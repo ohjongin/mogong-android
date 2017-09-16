8802e1b6319444c8ea08f89f5e229891820d752c
seo <bravoyourlife528@gamil.com>
2017-09-16 13:49:51 +0900

    AlramReceiver notification

---
+++ app/src/main/java/org/androidtown/alram_push/AlramReceiver.java
@@ -0,0 +1,40 @@
+package org.androidtown.alram_push;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
+import android.widget.Toast;
+
+/**
+ * Created by samsung on 2017-09-16.
+ */
+
+public class AlramReceiver extends BroadcastReceiver
+{
+    private static final int MY_NOTIFICATION_ID = 0;
+    NotificationManager notificationManager;
+    Notification myNotification;
+
+    @Override
+    public void onReceive(Context context, Intent intent)
+    {
+        Toast.makeText(context, "Time is set", Toast.LENGTH_SHORT).show();
+        Intent myIntent = new Intent(context, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, myIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        myNotification = new NotificationCompat.Builder(context)
+                .setContentTitle("모두의 공강")
+                .setContentText("10분전입니다.")
+                .setWhen(System.currentTimeMillis())
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(true)
+                .build();
+
+        notificationManager = (NotificationManager)context.getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(MY_NOTIFICATION_ID, myNotification);
+    }
+}
--- app/src/main/java/org/androidtown/alram_push/MainActivity.java
+++ app/src/main/java/org/androidtown/alram_push/MainActivity.java
@@ -1,13 +1,32 @@
 package org.androidtown.alram_push;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import android.app.Activity;
+import android.view.Menu;
+import android.webkit.WebView;
+import android.widget.DatePicker;
+import android.widget.Toast;
+
 public class MainActivity extends AppCompatActivity {
+    DatePicker datePicker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        datePicker = (DatePicker) findViewById(R.id.datepicker);
+
+        datePicker.init(datePicker.getYear(), datePicker.getMonth(), datePicker.getDayOfMonth(), new DatePicker.OnDateChangedListener() {
+
+            @Override
+            public void onDateChanged(DatePicker view, int year, int monthOfYear, int dataOfMonth) {
+                String msg = String.format("%d / %d / %d", year, monthOfYear + 1, dataOfMonth);
+                Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
